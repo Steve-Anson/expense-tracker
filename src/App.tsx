e6dd348a71697c229906cfe4442720ab1cdeb8ca
Steve-Anson <steveanson12@gmail.com>
2023-04-22 13:14:46 +0530

    Build ExpenseList Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import ExpenseList from "./components/ExpenseList";
 
 const App = () => {
-  return <div>App</div>;
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Milk", amount: 10, category: "Groceries" },
+    { id: 2, description: "Cheese", amount: 20, category: "Groceries" },
+    { id: 3, description: "Butter", amount: 30, category: "Groceries" },
+    { id: 4, description: "Electricity", amount: 40, category: "Utilities" },
+  ]);
+
+  return (
+    <div>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      ></ExpenseList>
+    </div>
+  );
 };
 
 export default App;
